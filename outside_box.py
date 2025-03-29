ca3412815d1c1c5666b96a18583975dc29c6e375
Thomas-Ishida <ishida.thomas@hotmail.com>
2025-03-29 15:06:20 +0900

    update

---
+++ outside_box.py
@@ -0,0 +1,104 @@
+import pygame
+import sys
+import pygame.font
+
+pygame.init()
+screen_width, screen_height = 800, 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Maze")
+font = pygame.font.Font(None,size=50)
+
+LIFETIME = 1500 
+SQUARE_COLOR = (255, 255, 255)  
+bridge = False
+won = False
+
+mouse_x, mouse_y = pygame.mouse.get_pos()
+
+path = [((0, 600), (120, 10)),
+        ((120, 120), (10, 490)),
+        ((100, 120), (20, 10)),
+        ((50, 100), (50, 50)),
+        ((400, 400), (50, 50)),
+        ((750, 700), (50, 50))]
+
+    
+endpoint_x = 425;
+endpoint_y = 425;
+endpoint_radius = 10; 
+
+rectangle_x = 50
+rectangle_y = 50
+
+# def add_rectangle(path, x, y, width, height):
+#     timestamp = pygame.time.get_ticks()
+#     path.append(((x , y - height),(width,height))
+
+msg = font.render("Think Outside The Box...",1,(200,0,0))
+# msg2 = font.render("Creating bridges is the 'right' way...",1,(0,200,0))
+msg3 = font.render("You Win!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",1,(0,200,0))
+msg4 = font.render("I didn't think you'd be 'that' smart...",1,(255,165,0))
+# msg5 = font2.render("  (Don't worry, it's a checkpoint)",1,(128,128,128))
+# msg6 = font2.render("Sike! it isn't",1,(128,128,128))
+
+run = True
+while run:
+    screen.fill((0, 0, 0))
+    if won == False :
+        screen.blit(msg,(150,100))
+    else :
+        screen.blit(msg3,(300,300))
+        
+    if bridge == True: 
+        screen.blit(msg4,(100,700))
+        path.append((((750,50),(50,50))))
+ 
+    
+
+        
+    for rect in path:
+        pygame.draw.rect(screen, (255, 255, 255), rect)
+        
+    # if(won == False):
+    #     for i in range(len(path) - 1, -1, -1):  # Iterate in reverse order
+    #         rect, size, timestamp = path[i]
+    #         if current_time - timestamp < LIFETIME:
+    #             pygame.draw.rect(screen, SQUARE_COLOR, (rect[0] ,rect[1], size[0], size[1]))
+    #         else:
+    #             path.pop(i)
+        
+    pygame.draw.circle(screen, (255,0,0),(endpoint_x , endpoint_y),endpoint_radius)
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.flip()
+    if ((pygame.mouse.get_pos()[0]-endpoint_x)**2+(pygame.mouse.get_pos()[1]-endpoint_y)**2)<=endpoint_radius**2:
+        print("you won")
+        won = True
+        
+    if (pygame.mouse.get_pos()[0] >= 750 and pygame.mouse.get_pos()[0] <= 800) and (pygame.mouse.get_pos()[1] >= 700 and pygame.mouse.get_pos()[1] <= 750):
+        bridge = True
+
+    #check if you're out of bounds
+    buffer = 1  # 1-pixel border
+
+    if any(
+        (b[0][0] - buffer) < pygame.mouse.get_pos()[0] < (b[1][0] + b[0][0] + buffer) and 
+        (b[0][1] - buffer) < pygame.mouse.get_pos()[1] < (b[1][1] + b[0][1] + buffer) 
+        for b in path
+    ):
+        print("True")
+    else:
+        print("False")
+        if not won:
+            pygame.mouse.set_pos((75, 125))
+    # if any(b[0][0]<pygame.mouse.get_pos()[0]<b[1][0]+b[0][0] and b[0][1]<pygame.mouse.get_pos()[1]<b[1][1]+b[0][1] for b in path):
+    #     print("True")
+    # else:
+    #     print("False")
+    #     if(won == False):
+    #         pygame.mouse.set_pos((75, 125))
+    pygame.display.update()
